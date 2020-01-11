ccc7fe1c92be79f0977b9f2245c087090f34a456
granger kenolane <kenolane28@gmail.com>
2020-01-11 22:46:45 +0100

    Auto stash before rebase of "master"

--- dmm/src/Controller/DefaultController.php
+++ dmm/src/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -15,5 +18,15 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
+    }
+
+    /**
+     * @Route("/{category}", name="default_categorie")
+     */
+    public function category(ArticleRepository $repository, CategoryRepository $categoryRepository, Request $request )
+    {
+        $articles = $repository->findAll();
+
+        return $this->render('frontend/category.html.twig', 'article' => $article);
     }
 }
