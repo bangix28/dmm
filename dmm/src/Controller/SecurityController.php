73e9fca044ae8071d0c0610ff37fc142f372f77b
granger kenolane <kenolane28@gmail.com>
2020-01-24 22:06:27 +0100

    Create securityController

---
+++ dmm/src/Controller/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Services\Security\LoginServices;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/security", name="security")
+     */
+    public function index()
+    {
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login()
+    {
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/inscription", name="security_registration")
+     */
+    public function registration(LoginServices $loginServices, Request $request )
+    {
+        $form = $loginServices->formCreate($request);
+        if ($form === true)
+        {
+            return $this->redirectToRoute('security_login');
+        }
+        return $this->render('security/registrationView.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function logout() {}
+}
