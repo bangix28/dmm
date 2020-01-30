006f3832db4a28002ee1cdc4362d9ffe1eba4b0b
granger kenolane <kenolane28@gmail.com>
2020-01-30 18:07:15 +0100

    add class  for trumbowyg

--- dmm/src/Form/ArticleType.php
+++ dmm/src/Form/ArticleType.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
