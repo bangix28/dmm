e8e35901225e609c5e3a3f669c34b281111e54fb
granger kenolane <kenolane28@gmail.com>
2020-03-25 20:33:56 +0100

    create a new auth with symfony

--- dmm/src/Controller/SecurityController.php
+++ dmm/src/Controller/SecurityController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Services\Security\LoginServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 class SecurityController extends AbstractController
@@ -20,19 +22,14 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/login", name="security_login")
-     */
-    public function login( )
-    {
-        return $this->render('security/login.html.twig');
-    }
+
 
     /**
      * @Route("/inscription", name="security_registration")
      */
     public function registration(LoginServices $loginServices, Request $request )
     {
+
         $form = $loginServices->formCreate($request);
         if ($form === true)
         {
@@ -42,10 +39,27 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/deconnexion", name="security_logout")
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
      */
     public function logout()
     {
-
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 }
