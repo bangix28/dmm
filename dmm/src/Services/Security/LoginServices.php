5980838c1a408fe17c2a7505d746607e0be4f814
granger kenolane <kenolane28@gmail.com>
2020-04-17 20:48:02 +0200

    Login by email now

---
+++ dmm/src/Services/Security/LoginServices.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\Services\Security;
+
+
+use App\Form\RegistrationFormType;
+use App\Form\RegistrationType;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+
+class LoginServices extends AbstractController
+{
+    private $manager;
+
+    private $encoder;
+
+    public function __construct(EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $this->manager = $manager;
+        $this->encoder = $encoder;
+    }
+
+    public function formCreate(Request $request, $edit)
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->validationForm($user);
+            $form = true;
+            return $form;
+        }
+        return $form;
+    }
+
+    public function validationForm(User $user)
+    {
+        $pass_hach = $this->encoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($pass_hach);
+        $user->setRoles(['ROLES_USER']);
+        $user->setImages('user.png');
+        $this->manager->persist($user);
+        $this->manager->flush();
+    }
+
+
+
+}
