1359fe9cb43727a74f49e48ef7e416b2cc1a19c8
granger kenolane <kenolane28@gmail.com>
2020-04-30 18:45:51 +0200

    js add

---
+++ dmm/assets/js/form.js
@@ -0,0 +1,6 @@
+let a = document.getElementById('btnSubmit');
+a.addEventListener("submit", function () {
+    a.delayTime;
+    a.reset();  // Reset all form data
+    return false; // Prevent page refresh
+});
