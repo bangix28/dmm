d1f614ac3db81ad665ae0578daeffccd6fd013b1
granger kenolane <kenolane28@gmail.com>
2020-05-24 15:03:01 +0200

    Add userInterface

--- dmm/src/Controller/DefaultController.php
+++ dmm/src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,8 +18,9 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(UserInterface $user)
     {
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
