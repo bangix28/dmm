0285f494c7b6d05cbe1f1ff0f8bb727f1f9cd2d7
granger kenolane <kenolane28@gmail.com>
2020-05-31 21:30:54 +0200

    create verification follow

--- dmm/src/Services/Profil/ProfilServices.php
+++ dmm/src/Services/Profil/ProfilServices.php
@@ -3,44 +3,61 @@
 
 namespace App\Services\Profil;
 
-
 use App\Entity\Follow;
 use App\Entity\User;
 use App\Form\FollowType;
+use App\Repository\FollowRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProfilServices extends AbstractController
 {
 
     private $manager;
 
-    public function __construct(EntityManagerInterface $manager)
+    private $followRepository;
+
+    public function __construct(EntityManagerInterface $manager, FollowRepository $followRepository)
     {
         $this->manager = $manager;
+        $this->followRepository = $followRepository;
     }
 
     public function follow(Request $request, $user)
     {
-        dump($request->get('id'));
         $follow = new Follow();
         $form = $this->createForm(FollowType::class, $follow);
         $form->handleRequest($request);
         if ($form->isSubmitted())
         {
-            $this->validationForm($follow,$user, $request);
+            $followId = $request->get('id');
+            $this->verification($user, $followId, $request, $follow);
+
         }
         return $form;
     }
 
-    public function validationForm(Follow $follow,User $user, Request $request)
+    public function addFollow(Follow $follow,User $user, Request $request, $followId)
     {
         $follow->setFollowedAt(new \DateTime('now'));
-       $follow->setFollowedId($request->get('id'));
+       $follow->setFollowedId($followId);
        $follow->setFollower($user);
        $this->manager->persist($follow);
        $this->manager->flush();
        return $this->redirectToRoute('default');
+    }
+
+    public function verification( User $user, $followId, $request, $follow)
+    {
+        $verification = $this->followRepository->findBy(array('follower' => $this->getUser(), 'followedId' => $followId));
+        dump($verification);
+         if (empty($verification))
+         {
+             $this->addFollow($follow, $user, $request, $followId);
+         }else{
+            echo 'rempli !';
+         }
     }
 }
